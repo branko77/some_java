99d6de892b1b6e50ab88118ce1ddfd683e5d4341
root <root@ip-172-31-17-240.eu-north-1.compute.internal>
2020-03-27 12:15:01 +0000

    test

--- test/hellotest.java
+++ test/hellotest.java
@@ -11,8 +11,8 @@ public class hellotest {
 
     @Test
     public void testmethod() {   
-	string first="one";
-	string second="one";     
+	string first="one"
+	string second="one"     
         assertEquals(first,second);
     }
 
